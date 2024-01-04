96505b50dc9921c4ff121a571750777c3fe00d87
ARTYOM <109017029+mark-artyom@users.noreply.github.com>
2024-01-04 21:16:53 +0300

    feat: sorting table and discount on product

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+table1.onclick = function(e) {
+    if (e.target.tagName != 'TH') return
+    let th = e.target
+    sortTable(th.cellIndex, th.dataset.type, 'table1')
+}
+
+table2.onclick = function(e) {
+    if (e.target.tagName != 'TH') return
+    let th = e.target
+    sortTable(th.cellIndex, th.dataset.type, 'table2')
+}
+
+function sortTable(colNum, type, id) {
+    let elem = document.getElementById(id)
+    let tBody = elem.querySelector('tbody')
+    let rowsArray = Array.from(tBody.rows)
+    let compare
+    switch (type) {
+        case 'number':
+            compare = function (rowA, rowB) {
+                return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+            };
+            break;
+        case 'string':
+            compare = function (rowA, rowB) {
+                return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1;
+            };
+            break;
+        }
+        rowsArray.sort(compare)
+        tBody.append(...rowsArray)
+}
+
 if (!localStorage.getItem('goods')) {
     localStorage.setItem('goods', JSON.stringify([]))
 }
@@ -67,8 +100,8 @@ function updateGoods() {
                     <td class="price-name">${goods[i][1]}</td>
                     <td class="price-one">${goods[i][2]}</td>
                     <td class="price-count">${goods[i][4]}</td>
-                    <td class="price-discount"><input data-goodId="${goods[i][0]}" type="text" value="${goods[i][5]}" min="0" max="100"></td>
-                    <td>${goods[i][4]}</td>
+                    <td class="price-discount"><input data-goodid="${goods[i][0]}" type="text" value="${goods[i][5]}" min="0" max="100"></td>
+                    <td>${goods[i][6]}</td>
                     <td><button class="good-delete btn-danger" data-delete = ${goods[i][0]}>&#10006;</button></td>
                 </tr>
                 `
@@ -141,6 +174,24 @@ document.querySelector('.cart').addEventListener('click', function(e) {
             goods[i].splice(4, 1, goods[i][4] - 1)
             localStorage.setItem('goods', JSON.stringify(goods))
             updateGoods()
+        }
+    }
+})
+
+document.querySelector('.cart').addEventListener('input', function(e) {
+    if (!e.target.dataset.goodid) {
+        return
+    }
+    let goods = JSON.parse(localStorage.getItem('goods'))
+    for (let i = 0; i < goods.length; i++) {
+        if(goods[i][0] == e.target.dataset.goodid) {
+            goods[i][5] = e.target.value
+            goods[i][6] = goods[i][4] * goods[i][2] - goods[i][4] * goods[i][2] * goods[i][5] * 0.01
+            localStorage.setItem('goods', JSON.stringify(goods))
+            updateGoods()
+            let input = document.querySelector(`[data-goodid="${goods[i][0]}"]`)
+            input.focus()
+            input.selectionStart = input.value.length
         }
     }
 })
